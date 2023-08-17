1591d4ad13d713931c1fc712df8930bab4d83729
Ed <Siniakov>
2023-08-17 17:38:23 +0300

    in progress

--- FuckingNE/PacketTunnelProvider.swift
+++ FuckingNE/PacketTunnelProvider.swift
@@ -8,30 +8,174 @@
 import NetworkExtension
 import OpenVPNAdapter
 
+extension NEPacketTunnelFlow: OpenVPNAdapterPacketFlow {}
+
 class PacketTunnelProvider: NEPacketTunnelProvider {
 
+    lazy var vpnAdapter: OpenVPNAdapter = {
+        let adapter = OpenVPNAdapter()
+        adapter.delegate = self
+
+        return adapter
+    }()
+
+    let vpnReachability = OpenVPNReachability()
+
+    var startHandler: ((Error?) -> Void)?
+    var stopHandler: (() -> Void)?
+
     override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
-        // Add code here to start the process of connecting the tunnel.
+        guard
+            let protocolConfiguration = protocolConfiguration as? NETunnelProviderProtocol,
+            let providerConfiguration = protocolConfiguration.providerConfiguration
+        else {
+            fatalError()
+        }
+
+        guard let ovpnContent = providerConfiguration["ovpn"] as? String else {
+            fatalError()
+        }
+
+        let configuration = OpenVPNConfiguration()
+        configuration.fileContent = ovpnContent.data(using: .utf8)
+        configuration.settings = [:]
+
+        configuration.tunPersist = true
+
+        let evaluation: OpenVPNConfigurationEvaluation
+        do {
+            evaluation = try vpnAdapter.apply(configuration: configuration)
+        } catch {
+            completionHandler(error)
+            return
+        }
+
+        if !evaluation.autologin {
+            guard let username: String = protocolConfiguration.username else {
+                fatalError()
+            }
+
+            guard let password: String = providerConfiguration["password"] as? String else {
+                fatalError()
+            }
+
+            let credentials = OpenVPNCredentials()
+            credentials.username = username
+            credentials.password = password
+
+            do {
+                try vpnAdapter.provide(credentials: credentials)
+            } catch {
+                completionHandler(error)
+                return
+            }
+        }
+
+        vpnReachability.startTracking { [weak self] status in
+            guard status == .reachableViaWiFi else { return }
+            self?.vpnAdapter.reconnect(afterTimeInterval: 5)
+        }
+
+        startHandler = completionHandler
+        vpnAdapter.connect(using: packetFlow)
     }
-    
+
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        // Add code here to start the process of stopping the tunnel.
-        completionHandler()
+        stopHandler = completionHandler
+
+        if vpnReachability.isTracking {
+            vpnReachability.stopTracking()
+        }
+
+        vpnAdapter.disconnect()
     }
+
+}
+
+extension PacketTunnelProvider: OpenVPNAdapterDelegate {
     
-    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
-        // Add code here to handle the message.
-        if let handler = completionHandler {
-            handler(messageData)
+    func openVPNAdapter(_ openVPNAdapter: OpenVPNAdapter, configureTunnelWithNetworkSettings networkSettings: NEPacketTunnelNetworkSettings?, completionHandler: @escaping (Error?) -> Void) {
+        networkSettings?.dnsSettings?.matchDomains = [""]
+
+        setTunnelNetworkSettings(networkSettings, completionHandler: completionHandler)
+    }
+
+    func openVPNAdapter(_ openVPNAdapter: OpenVPNAdapter, handleEvent event: OpenVPNAdapterEvent, message: String?) {
+        switch event {
+        case .connected:
+            if reasserting {
+                reasserting = false
+            }
+
+            guard let startHandler = startHandler else { return }
+
+            startHandler(nil)
+            self.startHandler = nil
+
+        case .disconnected:
+            guard let stopHandler = stopHandler else { return }
+
+            if vpnReachability.isTracking {
+                vpnReachability.stopTracking()
+            }
+
+            stopHandler()
+            self.stopHandler = nil
+
+        case .reconnecting:
+            reasserting = true
+
+        default:
+            break
         }
     }
-    
-    override func sleep(completionHandler: @escaping () -> Void) {
-        // Add code here to get ready to sleep.
-        completionHandler()
+
+    func openVPNAdapter(_ openVPNAdapter: OpenVPNAdapter, handleError error: Error) {
+        guard let fatal = (error as NSError).userInfo[OpenVPNAdapterErrorFatalKey] as? Bool, fatal == true else {
+            return
+        }
+
+        if vpnReachability.isTracking {
+            vpnReachability.stopTracking()
+        }
+
+        if let startHandler = startHandler {
+            startHandler(error)
+            self.startHandler = nil
+        } else {
+            cancelTunnelWithError(error)
+        }
     }
-    
-    override func wake() {
-        // Add code here to wake up.
+
+    func openVPNAdapter(_ openVPNAdapter: OpenVPNAdapter, handleLogMessage logMessage: String) {
     }
+
 }
+
+//class PacketTunnelProvider: NEPacketTunnelProvider {
+//
+//    override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+//        // Add code here to start the process of connecting the tunnel.
+//    }
+//
+//    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+//        // Add code here to start the process of stopping the tunnel.
+//        completionHandler()
+//    }
+//
+//    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
+//        // Add code here to handle the message.
+//        if let handler = completionHandler {
+//            handler(messageData)
+//        }
+//    }
+//
+//    override func sleep(completionHandler: @escaping () -> Void) {
+//        // Add code here to get ready to sleep.
+//        completionHandler()
+//    }
+//
+//    override func wake() {
+//        // Add code here to wake up.
+//    }
+//}
--- FuckingVPN/ViewController.swift
+++ FuckingVPN/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import NetworkExtension
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    var providerManager: NETunnelProviderManager!
 
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            loadProviderManager {
+                ///  "127.0.0.1"
+                self.configureVPN(serverAddress: "192.145.124.3", username: "", password: "")
+            }
+         }
+
+        func loadProviderManager(completion:@escaping () -> Void) {
+           NETunnelProviderManager.loadAllFromPreferences { (managers, error) in
+               if error == nil {
+                   self.providerManager = managers?.first ?? NETunnelProviderManager()
+                   completion()
+               }
+           }
+        }
+
+        func configureVPN(serverAddress: String, username: String, password: String) {
+          providerManager?.loadFromPreferences { error in
+             if error == nil {
+                let tunnelProtocol = NETunnelProviderProtocol()
+                 
+                 let configData = tunnelProtocol.providerConfiguration
+                 
+                 
+                 
+                tunnelProtocol.username = username
+                tunnelProtocol.serverAddress = serverAddress
+                tunnelProtocol.providerBundleIdentifier = "kanevsky.Beast-VPN.FuckingVPN"
+                tunnelProtocol.providerConfiguration = ["ovpn": "configData", "username": username, "password": password]
+                tunnelProtocol.disconnectOnSleep = false
+                self.providerManager.protocolConfiguration = tunnelProtocol
+                self.providerManager.localizedDescription = "Light VPN"
+                self.providerManager.isEnabled = true
+                self.providerManager.saveToPreferences(completionHandler: { (error) in
+                      if error == nil  {
+                         self.providerManager.loadFromPreferences(completionHandler: { (error) in
+                             do {
+                               try self.providerManager.connection.startVPNTunnel()
+                             } catch let error {
+                                 print(error.localizedDescription)
+                             }
+                         })
+                      }
+                })
+              }
+           }
+        }
 
 }
 
