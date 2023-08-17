47ae3087ecc47cf172731d91a73c5196c87cb06b
Ed <Siniakov>
2023-08-17 16:58:34 +0300

    Initial Commit

---
+++ FuckingVPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FuckingVPN
+//
+//  Created by Eduard Kanevskii on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
